122b22d95685b0087e5b4e62b418c1e4f995c674
jason1642 <jason1642@gmail.com>
2023-04-07 18:19:57 -0400

    Package update

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,27 +1,12 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-// import packageJson from './package.json' assert { type: "json" };
-
-import postcss from "rollup-plugin-postcss";
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
+import postcss from "rollup-plugin-postcss"
 
 const config =  
   {
     input: "src/index.ts",
-    // output: 
-    // // [
-    //   {
-    //     dir: "dist/index.d.ts",
-    //     format: "cjs",
-    //     sourcemap: true,
-    //   },
-    //   {
-    //     dir: "dist/esm/index.ts",
-    //     format: "esm",
-    //     sourcemap: true,
-    //   },
-    // ],
+
     plugins: [
       resolve(),
       commonjs(),
@@ -29,13 +14,7 @@ const config =
       postcss(), 
     ],
   }
-  // {
-  //   input: "dist/esm/types/index.d.ts",
-  //   output: [{ file: "dist/index.d.ts", format: "esm" }],
-  //   plugins: [dts()],
-  //   external: [/\.css$/],
 
-  // }
 
 
 export default config
