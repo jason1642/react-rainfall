a0568283eabffd4c56a78fb5f17ce9f13806f347
jason1642 <jason1642@gmail.com>
2023-04-07 18:57:51 -0400

    Updated rollup configurations

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,71 @@
-import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs"
-import typescript from "@rollup/plugin-typescript"
-import postcss from "rollup-plugin-postcss"
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import packageJson from './package.json' assert { type: "json" };
 
-const config =  
+import postcss from "rollup-plugin-postcss";
+
+const globals = { react: 'React' };
+
+const defaults = {
+	input: 'src/index.ts',
+	external: [
+		...Object.keys(packageJson.devDependencies || {})
+	],
+	plugins: [
+		typescript()
+	]
+}
+
+const config =  [
   {
     input: "src/index.ts",
-
+    output: 
+    [
+      {
+        dir: "dist",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        dir: "dist",
+        format: "esm",
+        sourcemap: true,
+      }, 
+    ],
     plugins: [
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ declaration: true, declarationDir: 'dist', tsconfig: "./tsconfig.json" }),
       postcss(), 
     ],
-  }
+  },
+  {
+    input: "dist/esm/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+    external: [/\.css$/],
 
-
+  },
+];
 
 export default config
+
+// export default [
+// 	{
+// 		...defaults,
+// 		output: [
+// 			{ file: pkg.main, format: 'cjs', globals },
+// 			{ file: pkg.browser, name: 'lib', format: 'iife', globals }
+// 		]
+// 	}, {
+// 		...defaults,
+// 		plugins: [
+// 			typescript({ declaration: true, declarationDir: 'dist' })
+// 		],
+// 		output: [
+// 			{ dir: 'dist', format: 'es', globals }
+// 		]
+// 	}
+// ];
--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App  numDrops={25}/>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
