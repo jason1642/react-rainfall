128c49bd14c2937691fbf34d87a1edfa97677884
jason1642 <jason1642@gmail.com>
2023-04-09 19:09:02 -0400

    Rollup config fix

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
-export { default as Rain } from "./Rain/Rain";
+import Rain from './Rain/Rain'
+export default Rain;
 // This page can be used to set list of multiple export components so the 
 // user will only have to import by 'import {Rain} from 'react-rainfall' instead of 
 // having to specify the entire path to the export default component 
